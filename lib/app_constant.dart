cfad9d94625a29f43ec0136a3492e575ab0a476d
Mohsin <mohsin.flutter.pk@gmail.com>
2025-04-14 09:48:18 +0500

    Delete lib directory

--- lib/app_constant.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-class AppConstant{
-  static String appMainName='Ecommerce';
-  static String appPoweredBy='Powered BY Tech';
-  static const appMainColor=Color(0xffbf1b08);
-  static const appScendoryColor=Color(0xff981206);
-  static const appTextColor=Color(0xffFBF5F4);
-  static const appStatusBarColor=Color(0xffFBF5F4);
-}
